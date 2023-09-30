c7dcfe9f0a42a359c4f48676856ce2cce7b87900
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-30 07:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function curry(callback) {
- 
-  const args = [];
-  function curried(...newArgs) {
+   const args = [];
+
+   function curried(...newArgs) {
     args.push(...newArgs);
-	  if (newArgs.length === 0) {
+
+      if (newArgs.length === 0) {
       return callback(...args);
     }
 
-      return curried;
+     return curried;
   }
+
+    curried.valueOf = function () {
+    return callback(...args);
+  };
 
   return curried;
 }
